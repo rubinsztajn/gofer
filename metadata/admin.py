b0caa6997719879df9a23da4b9af659b49f62184
rubinsztajn <rubinsztajn@gmail.com>
2013-04-11 16:49:30 -0400

    Tweaked models and added some UI bits

--- metadata/admin.py
+++ metadata/admin.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect
 from django.core import serializers
 from import_export.admin import ImportExportMixin
 from imagekit.admin import AdminThumbnail
-from metadata.models import Record, Name, Image, Related
+from metadata.models import Record, Name, Image, Related, Tags
 
 def export_selected_objects(modeladmin, request, queryset):
     selected = request.POST.getlist(admin.ACTION_CHECKBOX_NAME)
@@ -15,11 +15,11 @@ class ImageInline(admin.TabularInline):
 
 class RecordAdmin(ImportExportMixin, admin.ModelAdmin):
     actions = [export_selected_objects]
-    fields = ('title', 'abstract', ('date_created', 'date_issued'), 'names', 'pages', 'size', 'relatives')
-    raw_id_fields = ('names','relatives',)
-    autocomplete_lookup_fields = {'m2m':['names', 'relatives'],}
+    fields = ('record_status', 'title', 'abstract', ('date_created', 'date_issued'), 'names', 'pages', 'size', 'relatives', 'tags', 'notes')
+    raw_id_fields = ('names','relatives','tags',)
+    autocomplete_lookup_fields = {'m2m':['names', 'relatives', 'tags'],}
     list_display = ('title', 'date_created')
-    search_fields = ('title', 'abstract')
+    search_fields = ('title', 'abstract', 'notes')
     inlines = [ImageInline,]
 
 class ImageAdmin(admin.ModelAdmin):
@@ -28,5 +28,6 @@ class ImageAdmin(admin.ModelAdmin):
 
 admin.site.register(Name)
 admin.site.register(Related)
+admin.site.register(Tags)
 admin.site.register(Image, ImageAdmin)
 admin.site.register(Record, RecordAdmin)
--- metadata/models.py
+++ metadata/models.py
@@ -15,7 +15,7 @@ class Name(models.Model):
     first_name = models.CharField(max_length=200, blank=True)
     last_name = models.CharField(max_length=200)
     dates = models.CharField(max_length=20, blank=True)
-    type = models.CharField(max_length=3, choices=TYPES)
+    type = models.CharField(max_length=3, choices=TYPES, blank=True)
     role = models.CharField(max_length=3, choices=ROLES, blank=True)
 
     @staticmethod
@@ -53,8 +53,23 @@ class Related(models.Model):
     def __unicode__(self):
         return "%s (%s)" % (self.title, self.identifier)
         
+class Tags(models.Model):
+    name = models.CharField(max_length=100)
+    
+    @staticmethod
+    def autocomplete_search_fields():
+        return ('name__icontains',)
+
+    def __unicode__(self):
+        return self.name
 
 class Record(models.Model):
+    STATUS = (
+        ('d', 'Draft'),
+        ('r', 'Ready for export'),
+        ('e', 'Exported'),
+    )
+
     title = models.TextField()
     abstract = models.TextField(blank=True)
     names = models.ManyToManyField(Name, blank=True)
@@ -62,13 +77,18 @@ class Record(models.Model):
     date_issued = models.CharField(max_length=100, blank=True)
     pages = models.CharField(max_length=10, blank=True)
     size = models.CharField(max_length=100, blank=True)
+    place = models.CharField(max_length=100, blank=True)
+    tags = models.ManyToManyField(Tags, blank=True)
     relatives = models.ManyToManyField(Related, blank=True)
+    notes = models.TextField(blank=True)
+    record_status = models.CharField(max_length=1, choices=STATUS, default=STATUS[0])
     
     def __unicode__(self):
         return self.title
 
     def get_thumb(self):
         if self.image_set.all():
+            # display the first image in the set if multiple
             return self.image_set.all()[0].thumbnail.url
         else:
             return None
@@ -78,7 +98,7 @@ class Image(models.Model):
     thumbnail = ImageSpecField([Adjust(contrast=1.2, sharpness=1.1), 
                                 ResizeToFill(50,50)], image_field='image',
                                 format='JPEG', options={'quality':90})
-    filename = models.CharField(max_length=100)
-    title = models.CharField(max_length=200, blank=True)
+    original_path = models.CharField(max_length=100, blank=True)
+    title = models.CharField(max_length=200)
     record = models.ForeignKey(Record, blank=True)
 
